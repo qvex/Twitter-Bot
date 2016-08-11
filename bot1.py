4c79f9dde50a2593105dd8d33d0935223d025fa0
Whatever <gaurav.singh93@hotmail.com>
2016-08-11 22:03:27 +0530

    Create bot1.py

---
+++ bot1.py
@@ -0,0 +1,14 @@
+import tweepy, time
+from credentials import *
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
+api = tweepy.API(auth)
+
+filename = open('chunk.txt', 'r')
+f = filename.readlines()
+filename.close()
+
+for line in f:
+    api.update_status(line)
+    print(line)
+    time.sleep(15)
